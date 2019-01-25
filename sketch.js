432e3b32bd754eb639e3d6744ade542ba785e61a
Janessa Tran <tranjanessa@gmail.com>
2019-01-24 21:20:03 -0600

    save initial changes

---
+++ sketch.js
@@ -0,0 +1,11 @@
+var containerDiv = document.getElementsByClassName("#grid-container")
+
+function createDivs(num) {
+    for(i = 0; i < num; i++) {
+        var newDiv = document.createElement("div");
+        newDiv.className = "grid-item";
+        document.getElementsByClassName("grid-container")[0].appendChild(newDiv);
+    }
+}
+
+createDivs(100);
