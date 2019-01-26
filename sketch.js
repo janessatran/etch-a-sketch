1b76f441ebdece481af82228ea7bd9287a3fb6ef
Janessa Tran <tranjanessa@gmail.com>
2019-01-26 10:27:57 -0600

    save changes

--- sketch.js
+++ sketch.js
@@ -1,12 +1,43 @@
-var containerDiv = document.getElementsByClassName("#grid-container")
-
-function createDivs(num) {
-    console.log(num);
-    for(i = 0; i < num; i++) {
+var containerDiv = document.getElementsByClassName("grid-container");
+var container = document.getElementsByClassName("container");
+var clearButton = document.getElementById("clear");
+let size = 32;
+function createDivs() {
+    for(let i = 0; i < size * size ; i++) {
         var newDiv = document.createElement("div");
         newDiv.className = "grid-item";
-        document.getElementsByClassName("grid-container")[0].appendChild(newDiv);
+        newDiv.id = i.toString();
+        newDiv.style.height = (600 / size) + "px";
+        newDiv.style.width = (700 / size) + "px";
+
+        newDiv.addEventListener('click', function() {
+            this.style.background = "grey";
+        });
+
+        newDiv.addEventListener('mousemove', function(e) {
+            if(e.buttons == 1) {
+                this.style.background = "grey";
+            }
+            if (e.buttons == 2) {
+                this.style.background = "#ffdfa0";
+            }
+        });
+        containerDiv[0].appendChild(newDiv);
+        // document.getElementsByClassName("grid-container")[0].appendChild(newDiv);
     }
 }
 
-createDivs(64);
+function clearGrid() {
+    while (containerDiv[0].firstChild) {
+        containerDiv[0].removeChild(containerDiv[0].firstChild);
+    }
+    createDivs();
+}
+
+clearButton.addEventListener('click', function() {
+    clearGrid();
+});
+
+
+
+createDivs();
